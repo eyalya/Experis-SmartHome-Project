4843d7790b4a0e5d837e52ac2f4f9b16e968afc6
h60.noam <you@example.com>
2020-03-15 10:42:19 +0200

    dispatcher  group wip

---
+++ include/dispatcher_group.hpp
@@ -0,0 +1,19 @@
+#ifndef DISPATCHER_GROUP_HPP
+#define DISPATCHER_GROUP_HPP
+
+namespace smartHome {
+namespace hub {
+
+class DispatcherGroup
+{
+public:
+
+
+private:
+}
+    
+} // namespace hub
+} // namespace smartHome
+
+
+#endif //DISPATCHER_GROUP_HPP
